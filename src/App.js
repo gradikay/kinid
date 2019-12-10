94a06d4f1bf91f76fc6376a6cd1ddde42561338d
gradikay <gradi_kayamba@yahoo.com>
2019-12-09 19:43:44 -0500

    adding file to master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Building KinId.space <strong>Coming Soon</strong>
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/gradikay/kinid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Glody Kabuyi &amp; Gradi Kayamba
-        </a>
-      </header>
-    </div>
+    return (
+
+        <nav>
+            <h1>This is the nav bar</h1>
+        </nav>
+
   );
 }
 
