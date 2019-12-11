2c7989a0ee84e01f7af48629a6f0e8d05397e759
gradikay <gradi_kayamba@yahoo.com>
2019-12-11 10:39:41 -0500

    Updates

---
+++ precache-manifest.4585aad2c00755d477879c6441b967d0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e6edafa8af02dd42ede7426d95d16e",
+    "url": "/gradikay/kinid/index.html"
+  },
+  {
+    "revision": "eec7e6243cd9a17a9ae8",
+    "url": "/gradikay/kinid/static/css/main.ee96dd2b.chunk.css"
+  },
+  {
+    "revision": "d4c8494c7ccac008a583",
+    "url": "/gradikay/kinid/static/js/2.a3019b0f.chunk.js"
+  },
+  {
+    "revision": "dfeea506dedb1f8ad64e8d3829765f8f",
+    "url": "/gradikay/kinid/static/js/2.a3019b0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eec7e6243cd9a17a9ae8",
+    "url": "/gradikay/kinid/static/js/main.2039124a.chunk.js"
+  },
+  {
+    "revision": "bc7b73f4cc813de07131",
+    "url": "/gradikay/kinid/static/js/runtime-main.72ea6310.js"
+  },
+  {
+    "revision": "1bb8ca2b6b69d2817b6e50a315a6f0be",
+    "url": "/gradikay/kinid/static/media/21178.1bb8ca2b.jpg"
+  },
+  {
+    "revision": "9d9287b59e479108bd4be8d687c27061",
+    "url": "/gradikay/kinid/static/media/23260.9d9287b5.jpg"
+  },
+  {
+    "revision": "f4893935a7f777feaa5a9164bff3e95e",
+    "url": "/gradikay/kinid/static/media/2730921.f4893935.jpg"
+  },
+  {
+    "revision": "c71bf16f82eb50e8901083a9331d2900",
+    "url": "/gradikay/kinid/static/media/2782405.c71bf16f.jpg"
+  },
+  {
+    "revision": "fa741e5722fb18b77bab0d29404edbdf",
+    "url": "/gradikay/kinid/static/media/35976.fa741e57.jpg"
+  },
+  {
+    "revision": "2da6fa8d9180624fa442cb0b5a53d709",
+    "url": "/gradikay/kinid/static/media/congo.2da6fa8d.png"
+  },
+  {
+    "revision": "3cf3bf69f40720ab17f6ab0e2239e901",
+    "url": "/gradikay/kinid/static/media/logo.3cf3bf69.png"
+  },
+  {
+    "revision": "ef1581aa8025a3721280a913f3c08fa8",
+    "url": "/gradikay/kinid/static/media/scrum.ef1581aa.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kinid/precache-manifest.d881798193f4321de8a5759b42d68d13.js"
+  "/gradikay/kinid/precache-manifest.4585aad2c00755d477879c6441b967d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kinid/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gradikay/kinid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
