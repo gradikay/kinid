39b825b5fb0e3d534a7454c1a3a9c8aad3cd4394
gradikay <gradi_kayamba@yahoo.com>
2019-12-12 13:24:51 -0500

    Updates

---
+++ precache-manifest.f30a3d57fd5c53ab418938bec9736e8c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609db218a606aebf66b1b57342d2ff87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e350dc5a856f333bf68e",
+    "url": "/static/css/main.c5ed6e9e.chunk.css"
+  },
+  {
+    "revision": "d4c8494c7ccac008a583",
+    "url": "/static/js/2.a3019b0f.chunk.js"
+  },
+  {
+    "revision": "dfeea506dedb1f8ad64e8d3829765f8f",
+    "url": "/static/js/2.a3019b0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e350dc5a856f333bf68e",
+    "url": "/static/js/main.cc836665.chunk.js"
+  },
+  {
+    "revision": "663b8f47993b9fcd1a27",
+    "url": "/static/js/runtime-main.4b9046a4.js"
+  },
+  {
+    "revision": "1bb8ca2b6b69d2817b6e50a315a6f0be",
+    "url": "/static/media/21178.1bb8ca2b.jpg"
+  },
+  {
+    "revision": "9d9287b59e479108bd4be8d687c27061",
+    "url": "/static/media/23260.9d9287b5.jpg"
+  },
+  {
+    "revision": "f4893935a7f777feaa5a9164bff3e95e",
+    "url": "/static/media/2730921.f4893935.jpg"
+  },
+  {
+    "revision": "c71bf16f82eb50e8901083a9331d2900",
+    "url": "/static/media/2782405.c71bf16f.jpg"
+  },
+  {
+    "revision": "fa741e5722fb18b77bab0d29404edbdf",
+    "url": "/static/media/35976.fa741e57.jpg"
+  },
+  {
+    "revision": "2da6fa8d9180624fa442cb0b5a53d709",
+    "url": "/static/media/congo.2da6fa8d.png"
+  },
+  {
+    "revision": "3cf3bf69f40720ab17f6ab0e2239e901",
+    "url": "/static/media/logo.3cf3bf69.png"
+  },
+  {
+    "revision": "ef1581aa8025a3721280a913f3c08fa8",
+    "url": "/static/media/scrum.ef1581aa.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gradikay/kinid/precache-manifest.4585aad2c00755d477879c6441b967d0.js"
+  "/precache-manifest.f30a3d57fd5c53ab418938bec9736e8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gradikay/kinid/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
